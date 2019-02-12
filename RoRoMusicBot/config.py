39c0a5f852916d028e6d85ec474c89b46ebab8b4
kirillpavkin <kirillpavkin@gmail.com>
2019-02-12 14:56:04 +0300

    begin

---
+++ RoRoMusicBot/config.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+token = '730610480:AAGvFIXpeGb1vDAsuZrHUsGPHKLjq6y2A9Q'  # Токен бота
+database_name = 'music.db'  # Файл с базой данных
+shelve_name = 'shelve.db'  # Файл с хранилищем
