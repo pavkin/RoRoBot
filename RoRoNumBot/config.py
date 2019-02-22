9bc21394dfaf9307a97457165e64083580bdbc0d
kirillpavkin <kirillpavkin@gmail.com>
2019-02-22 13:31:51 +0300

    update

--- RoRoNumBot/config.py
+++ RoRoNumBot/config.py
@@ -10,18 +10,3 @@ database_table_columns = ['chat_id', 'max_score', 'game_start', 'game_time', 'ga
 settings = { 'easy': [5, 10, 15, 5],
              'medium': [10, 15, 10, 10],
              'hard': [15, 20, 5, 15] }
-
-left_easy = 5
-right_easy = 10
-time_seed_easy = 15
-random_seed_easy = 5
-
-left_medium = 10
-right_medium = 15
-time_seed_medium = 10
-random_seed_medium = 10
-
-left_hard = 15
-right_hard = 20
-time_seed_hard = 5
-random_seed_hard = 15
